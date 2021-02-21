d285770c6f2b59766b132fea48ea2baf90851560
gabriel-lamarao <gabriel.lamarao97@gmail.com>
2021-02-21 09:48:08 -0300

    Add Exercicio Empresa e Funcionario

--- main/Main.java
+++ main/Main.java
@@ -1,11 +1,32 @@
 package main;
 
-
-import unifap.listas.lista1.q9.Fatorial;
+import unifap.aulas.empresa.Empresa;
+import unifap.aulas.empresa.Funcionario;
 
 public class Main {
 	public static void main(String[] args) {
-		Fatorial f =  new Fatorial();
-		f.fatorial(8);
+		Empresa empresa = new Empresa();
+		Funcionario f1 = new Funcionario();
+		Funcionario f2 = new Funcionario();
+		Funcionario f3 = new Funcionario();
+		f1.setNome("Gabriel");
+		f1.setSalario(2100);
+		f1.setCpf("123456789-90");
+		f1.setCargo("Desenvolvedor");
+		empresa.adicionaFuncionario(f1);
+				
+		f2.setNome("mario");
+		f2.setSalario(3000);
+		f2.setCargo("Gerente");
+		f2.setCpf("098765432-12");
+		empresa.adicionaFuncionario(f2);
+		
+		f3.setNome("Carlos");
+		f3.setSalario(3000);
+		f3.setCargo("Gerente");
+		f3.setCpf("098765432-12");
+		empresa.adicionaFuncionario(f3);
+		empresa.getAll();
+		System.out.println(empresa.isFuncionario(f2));
 	}
 }
